12ad2a68646c211e87e4cfbb2d73161cb03d044b
PappAZsolt <pappzsolt918@gmail.com>
2023-02-07 22:56:50 +0200

    not completed

---
+++ OJTI_2022/PollutionMap/PollutionMap/Autentificare.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PollutionMap
+{
+    partial class Autentificare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dGVHarti = new System.Windows.Forms.DataGridView();
+            this.dGVMasurare = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVHarti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVMasurare)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dGVHarti
+            // 
+            this.dGVHarti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVHarti.Location = new System.Drawing.Point(12, 55);
+            this.dGVHarti.Name = "dGVHarti";
+            this.dGVHarti.Size = new System.Drawing.Size(350, 313);
+            this.dGVHarti.TabIndex = 0;
+            // 
+            // dGVMasurare
+            // 
+            this.dGVMasurare.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVMasurare.Location = new System.Drawing.Point(406, 66);
+            this.dGVMasurare.Name = "dGVMasurare";
+            this.dGVMasurare.Size = new System.Drawing.Size(640, 302);
+            this.dGVMasurare.TabIndex = 1;
+            // 
+            // Autentificare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PollutionMap.Properties.Resources.back6;
+            this.ClientSize = new System.Drawing.Size(1071, 563);
+            this.Controls.Add(this.dGVMasurare);
+            this.Controls.Add(this.dGVHarti);
+            this.Name = "Autentificare";
+            this.Text = "Autentificare";
+            ((System.ComponentModel.ISupportInitialize)(this.dGVHarti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVMasurare)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dGVHarti;
+        private System.Windows.Forms.DataGridView dGVMasurare;
+    }
+}
+
---
+++ OJTI_2022/PollutionMap/PollutionMap/Autentificare.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace PollutionMap
+{
+
+    public partial class Autentificare : Form
+    {
+        private StreamReader sr;
+        private SqlConnection con = null;
+        private SqlCommand cmd = null;
+        private string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Database1.mdf;Integrated Security=True;";
+        public Autentificare()
+        {
+
+            InitializeComponent();
+            con = new SqlConnection(conStr);
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            PopuleazaTabelaHarti();
+            SelectToateHartile();
+            PopuleazaTabelaMasurare();
+            SelectToateMasuratorile();
+
+        }
+        private void SelectToateHartile()
+        {
+            string query = PopuleazaTabelaHarti();
+            query = "SELECT * FROM Harti";
+            cmd.CommandText = query;
+            var dr = cmd.ExecuteReader(); ///pentru Select pt a se executa
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dr.Close(); // important sa inchizi!! 
+            dGVHarti.DataSource = dt;
+
+        }
+        private string PopuleazaTabelaHarti()
+        {
+            /// luam din resurse harti.txt
+            string path = Directory.GetCurrentDirectory() + @"\Resurse\harti.txt";
+            sr = new StreamReader(path);
+            string line = "";
+            string query = "INSERT INTO Harti(NumeHarta, FisierHarta) VALUES (@nume, @fisier);";
+            while((line = sr.ReadLine()) != null)
+            {
+                string[] s = line.Split('#');
+                string numeHarta = s[0];
+                string fisierHarta = s[1];
+                cmd.Parameters.AddWithValue("nume", numeHarta);
+                cmd.Parameters.AddWithValue("fisier", fisierHarta);
+                cmd.CommandText = query;
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery(); ///pentru Insert into, delete, update
+                cmd.Parameters.Clear();
+
+            }
+            sr.Close();
+            return query;
+        }
+        private void SelectToateMasuratorile()
+        {
+            string query = PopuleazaTabelaMasurare();
+            query = "SELECT * FROM Masurare";
+            cmd.CommandText = query;
+            var dr = cmd.ExecuteReader(); ///pentru Select pt a se executa
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            dr.Close(); // important sa inchizi!! 
+            dGVMasurare.DataSource = dt;
+
+        } 
+
+    private int GetIdHarti(string harta)
+        {
+            con.Close();
+            con = new SqlConnection(conStr);
+            con.Open();
+
+            string query = "SELECT IdHarta FROM Harti WHERE NumeHarta = '" + harta + " '";
+            cmd = new SqlCommand(query,con);
+            var dr = cmd.ExecuteReader();
+            dr.Read();
+            int id = int.Parse(dr[0].ToString());
+            dr.Close();
+            return id;
+
+        }
+        private string PopuleazaTabelaMasurare()
+        {
+            string path = Directory.GetCurrentDirectory() + @"\Resurse\masurari.txt";
+            sr = new StreamReader(path);
+            string line = "";
+            string query = "INSERT INTO Masurare(IdHarta, PozitieX, PozitieY, ValoareMasurare,DataMasurare) VALUES (@idHarta, @pozitieX, @pozitieY, @valoareMasurare, @data);";
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] s = line.Split('#');
+                string numeHarta = s[0];
+                int idHarta = GetIdHarti(numeHarta);
+                int pozitieX = int.Parse(s[1]);
+                int pozitieY = int.Parse(s[2]);
+                double valoareMasurare = double.Parse(s[3]);
+                DateTime dataMasurare = DateTime.ParseExact(s[4], "dd/MM/yyyy HH:mm", null); 
+                cmd.Parameters.AddWithValue("idHarta", idHarta);
+                cmd.Parameters.AddWithValue("pozitieX", pozitieX);
+                cmd.Parameters.AddWithValue("pozitieY", pozitieY);
+                cmd.Parameters.AddWithValue("valoareMasurare", valoareMasurare);
+                cmd.Parameters.AddWithValue("data", dataMasurare);
+                cmd.CommandText = query;
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery(); ///pentru Insert into, delete, update
+                cmd.Parameters.Clear();
+
+            }
+            sr.Close();
+            return query;
+        }
+        
+    }
+}
